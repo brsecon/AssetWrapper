995c16af23e0b90e922713d175a23df14516a0c6
brsecon <bariseconomics@gmail.com>
2025-04-27 10:18:24 +0300

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,8 +3,8 @@ import "@nomicfoundation/hardhat-toolbox";
 import "dotenv/config"; // .env dosyasındaki değişkenleri yüklemek için import et
 
 // Ortam değişkenlerinden değerleri al
-const sepoliaRpcUrl = process.env.SEPOLIA_RPC_URL || ""; // Eğer değişken yoksa boş string ata (TypeScript tip kontrolü için)
-const sepoliaPrivateKey = process.env.SEPOLIA_PRIVATE_KEY;
+const baseSepoliaRpcUrl = process.env.BASE_SEPOLIA_RPC_URL || ""; // Eğer değişken yoksa boş string ata (TypeScript tip kontrolü için)
+const baseSepoliaPrivateKey = process.env.BASE_SEPOLIA_PRIVATE_KEY;
 
 const polygonRpcUrl = process.env.POLYGON_RPC_URL || "";
 const polygonPrivateKey = process.env.POLYGON_PRIVATE_KEY;
@@ -26,18 +26,25 @@ const config: HardhatUserConfig = {
     },
 
     // Sepolia Testnet Ayarları
-    sepolia: {
+    /* sepolia: {
       url: sepoliaRpcUrl,
       // Özel anahtar tanımlıysa dizi içine al, değilse boş dizi ata
       accounts: sepoliaPrivateKey !== undefined ? [sepoliaPrivateKey] : [],
       chainId: 11155111, // Sepolia'nın Chain ID'si
-    },
+    }, */
 
     // Polygon Mainnet Ayarları (örnek)
     polygon: {
       url: polygonRpcUrl,
       accounts: polygonPrivateKey !== undefined ? [polygonPrivateKey] : [],
       chainId: 137, // Polygon Mainnet Chain ID'si
+    },
+
+    baseSepolia: {
+      url: baseSepoliaRpcUrl,
+      // Özel anahtar tanımlıysa dizi içine al, değilse boş dizi ata
+      accounts: baseSepoliaPrivateKey !== undefined ? [baseSepoliaPrivateKey] : [],
+      chainId: 84532, // Sepolia'nın Chain ID'si
     },
 
     // Başka ağları da buraya benzer şekilde ekleyebilirsin
